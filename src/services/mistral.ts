4346218b88cb9971f9ca1da6d2c7761a1e57c06a
ZenithSUS <jeranmerino147@gmail.com>
2025-04-23 09:38:25 +0800

    feat: refactor Mistral chat content into a constant for improved readability

--- src/services/mistral.ts
+++ src/services/mistral.ts
@@ -4,6 +4,9 @@ import { MistralChatResponse } from "../utils/types";
 const apiKey = import.meta.env.VITE_MISTRAL_API_KEY;
 const client = new Mistral({ apiKey: apiKey });
 
+const mistralContent =
+  'You are a helpful assistant. Answer the user\'s questions. Respond in a JSON format and this is the format: {"response": "your response", "status": "This is a word like success, failed or error", "code": 200}. This is the only format you can respond with. Clean the format to avoid errors and don\'t add unnecessary content. Only output the JSON. If the user asks for a list, just add a number and a next line to it. For example: 1. First item\n2. Second item\n3. Third item\n4. Fourth item. The output should only be a string in the response key and in the content key only JSON, nothing more.';
+
 export default async function chatResponse(message: string) {
   try {
     const data: MistralChatResponse = await mistralChat(message);
@@ -27,8 +30,7 @@ async function mistralChat(message: string) {
     messages: [
       {
         role: "system",
-        content:
-          'You are a helpful assistant. Answer the user\'s questions. Respond in a JSON format and this is the format: {"response": "your response", "status": "This is a word like success, failed or error", "code": 200} this is the only format you can respond and output exactly like the format clean the format to avoid errors and dont add neccesarry in the content only output the JSON.',
+        content: mistralContent,
       },
       { role: "user", content: message },
     ],
